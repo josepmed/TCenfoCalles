0ec696e094912c902ba4691b8d8b5c58b50173c2
josepmed <jpmcjpmc@gmail.com>
2012-02-14 15:23:50 -0600

    Generado proyecto. TCenfoCalles en paquete UICarretera es main.

---
+++ TCenfoCalles/src/uicarretera/TCenfoCalles.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uicarretera;
+
+/**
+ *
+ * @author josepmedrano
+ */
+public class TCenfoCalles {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
